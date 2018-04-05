47b1fa34fd35a6cc23e04d50b176752db44c35c5
Matěj <vas@email.com>
2018-04-05 15:15:23 +0200

    Dokonceni ukolu

--- index.php
+++ index.php
@@ -5,13 +5,43 @@
     <title>xd</title>
   </head>
   <body>
-    <h1>Proměné</h1>
+    <h1>Bezdomovec</h1>
 <?php
 
-    $age= ;
+    $vodkaPrice = 130 ;
+    $homelessMoney = 25 ;
+    $studentka = 17 ;
+    $Boi = 40 ;
+    $podnikatel = 100 ;
+    $nigger = 50
 
  ?>
 
+    <p> <?php echo"Bezdomovec má $homelessMoney Korun českých" ;
+        ?>
+    </p> <?php  $homelessMoney = $homelessMoney  +  $studentka ;
+           echo"Soucítící studentka, čekající na vlak, vkládá do čepice 17 Kč. Bezdomovec má $homelessMoney Korun českých. "  ;
+     ?>
+    <p> <?php $homelessMoney = $homelessMoney  +  $Boi ;
+           echo"Muž, který právě skončil úspěšně s přijímacím pracovním pohovorem, věnuje bezdomovci peníze v hodnotě 40 kč. Bezdomovec má $homelessMoney Korun českých." ;
+     ?>
+   </p>
+   <p> <?php $homelessMoney = $homelessMoney + $podnikatel ;
+      echo"Před vstupem do pendolina jde kolem bezdomovce bohatý podnikatel. Aby neměl těžkou peněženku zbaví se drobné bankovky v hodnotě 100 Kč. Bezdomovec má $homelessMoney Korun českých." ;
+    ?>
+   </p>
+   <p> <?php $homelessMoney = $homelessMoney - $nigger ;
+        echo"Kolem čepice bezdomovce se nenápadně prochází skupinka nepřizpůsobivých spoluobčanů a sebere mu z ní 50 Kč. Bezdomovec má $homelessMoney Korun českých. "  ;
+    ?>
+   </p>
+   <p><?php echo "Peníze bezdomovce před návštěvou večerky: $homelessMoney . ";
 
+    ?>
+   </p>
+   <?php if ($homelessMoney >= $vodkaPrice) { $homelessMoney =  $homelessMoney - $vodkaPrice ; ?>
+    Bezďák má vodku a  <?php } ?>
+   <?php
+    echo "zbytek bezdomovo zisku po návštěvě večerky: $homelessMoney" ;
+    ?>
   </body>
 </html>
